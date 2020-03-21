daa297b11c466ab9d8769176fead87d577efe480
unknown <a.utegenova@student.inha.uz>
2020-03-21 23:12:28 +0500

    lab7

---
+++ webpage/functions.php
@@ -0,0 +1,8 @@
+<?php
+	$isPost = $_SERVER['REQUEST_METHOD'] == 'POST';
+	$action = isset($_REQUEST['action'])?$_REQUEST['action']:'';
+	
+	function redirect($path) {
+		header('Location: ' . $path, true, 301);
+		exit();
+	}
